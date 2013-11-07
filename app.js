695d000a0f25a065742dc82d11b27e46cf2eeeff
Nelson Rodrigues <nelsonrodrigues@MacBook-Pro-de-Nelson.local>
2013-11-07 18:56:34 +0000

    Adicionar load

--- app.js
+++ app.js
@@ -42,10 +42,77 @@ Ext.define('Italbox.Viewport', {
                 xtype: 'imageviewer',
                 imageSrc: 'catalogo/pag8.jpeg'
             },
-            {
-                xtype: 'imageviewer',
-                imageSrc: 'catalogo/pag9.jpeg'
-            }
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag10.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag11.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag12.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag13.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag14.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag15.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag16.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag17.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag18.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag19.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag20.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag21.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag22.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag23.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag24.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag25.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag26.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag27.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag28.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag29.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag30.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag31.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag32.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag33.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag34.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag35.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag36.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag37.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag38.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag39.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag40.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag41.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag42.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag43.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag44.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag45.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag46.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag47.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag48.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag49.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag50.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag51.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag52.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag53.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag54.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag55.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag56.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag57.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag58.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag59.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag60.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag61.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag62.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag63.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag64.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag65.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag66.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag67.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag68.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag69.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag70.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag71.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag72.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag73.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag74.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag75.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag76.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag77.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag78.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag79.jpeg'},
+            {xtype: 'imageviewer', imageSrc: 'catalogo/pag80.jpeg'}
            
         ],
         listeners: {
@@ -88,27 +155,11 @@ Ext.define('Italbox.ViewportPanel', {
             cls: 'header',
             docked: 'top',
             items: [
-                {   
-                    iconMask:true,
-                    iconCls: 'delete',
-                    ui:      'plain',
-                    listeners: {
-                        tap: function() {
-                            Ext.getCmp('myCarousel').getActiveItem().onZoomIn();
-                        }
-                    }
-                },
                 {
                     align: 'right',
                     ui:      'plain',
                     xtype: 'button',
-                    cls: 'open-menu',
-                    listeners: {
-                        tap: function() {
-                            Ext.getCmp('myCarousel').getActiveItem().onZoomOut();
-                        }
-                    }
-
+                    cls: 'open-menu'
                 }
             ]
         }, {
@@ -124,6 +175,28 @@ Ext.application({
     views : [
         'Ext.ux.ImageViewer'
     ],
+    icon: {
+        '57': 'resources/icons/Icon.png',
+        '72': 'resources/icons/Icon~ipad.png',
+        '114': 'resources/icons/Icon@2x.png',
+        '144': 'resources/icons/Icon~ipad@2x.png'
+    },
+    startupImage: {
+        '320x460': 'resources/startup/Default.jpg', // Non-retina iPhone, iPod touch, and all Android devices
+        '640x920': 'resources/startup/640x920.png', // Retina iPhone and iPod touch
+        '640x1096': 'resources/startup/640x1096.png', // iPhone 5 and iPod touch (fifth generation)
+        '768x1004': 'resources/startup/768x1004.png', //  Non-retina iPad (first and second generation) in portrait orientation
+        '748x1024': 'resources/startup/748x1024.png', //  Non-retina iPad (first and second generation) in landscape orientation
+        '1536x2008': 'resources/startup/1536x2008.png', // : Retina iPad (third generation) in portrait orientation
+        '1496x2048': 'resources/startup/1496x2048.png' // : Retina iPad (third generation) in landscape orientation
+    },
+
+    isIconPrecomposed: false,
+
+    glossOnIcon: false,
+    phoneStartupScreen: 'resources/startup/italboxStartUp.jpg',
+    tabletStartupScreen: 'resources/startup/italboxStartUp.jpg',
+    
     launch: function() {
 
         Ext.Viewport.add({
