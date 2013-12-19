37af0c89fb834c004ca02511a82bc0d8c43395c3
rjgc <rj.leiria@gmail.com>
2013-12-19 18:57:19 +0000

    Bug Fix2

--- app.js
+++ app.js
@@ -784,11 +784,12 @@ Ext.define('Italbox.Viewport6', {
             direction: 'down',
             easing: 'easeIn'
         }, 
-                //cls: 'teste',
-        store: {id: 'produtos',
-                 data:tprodutos
-               },
-            
+        store:
+        {
+            id: 'produtos',
+            data:tprodutos
+        },
+        
         itemTpl:  '<div class="myContent">'+
             '<img src="'+caminho+'{foto}" style="float:left; width:60px; margin-right:20px;"></img>' +
             '<div>Nome: <b>{nome}</b></div>' +
